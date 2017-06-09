137db3f724896dd56b1f221a76c73f4c4adf3ff4
Scott Schapker <charles.schapker@viavisolutions.com>
2017-06-09 16:20:15 -0400

    Add ionic demo project.

---
+++ src/pages/browser/browser.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser, InAppBrowserObject, InAppBrowserOptions } from '@ionic-native/in-app-browser';
+import { Subscription } from 'rxjs/Rx';
+import { TabsPage } from '../tabs/tabs';
+
+const iabOptions: InAppBrowserOptions = {};
+
+@Component({
+    selector: 'browser-component',
+    template: ``
+})
+
+export class BrowserPage implements OnInit {
+    private browser: InAppBrowserObject;
+
+    constructor(private iab: InAppBrowser, private nav: NavController) { }
+
+    ngOnInit() {
+        this.openBrowser();
+    }
+
+    private openBrowser() {
+        this.browser = this.iab.create('http://google.com', '_blank', iabOptions);
+        this.addBrowserListeners();
+    }
+
+    private addBrowserListeners() {
+        const sub: Subscription = this.browser.on('exit').subscribe(() => {
+            this.nav.setRoot(TabsPage);
+            sub.unsubscribe();
+        });
+    }
+
+    
+}
+
